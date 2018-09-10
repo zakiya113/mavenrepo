a5acd6877047760d95e66905859ebc19f989a60b
minds9 <minds9@DESKTOP-9BMLCIN>
2018-09-10 11:26:47 +0530

    mvn repo

---
+++ com.mavendemo.interns/src/test/java/com/mavendemo/interns/mvnclass.java
@@ -0,0 +1,12 @@
+package com.mavendemo.interns;
+
+import org.testng.annotations.Test;
+
+public class mvnclass {
+
+	@Test()
+	public void print(){
+		System.out.println("This is mvn class");
+		System.out.println();
+	}
+}
